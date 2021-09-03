5d20676ef9f15fe60b2219e6ac01e8dec61587a7
Devesh Kumar <devesh2027@gmail.com>
2021-09-03 13:24:23 +0530

    Added functions to add and remove transparent overlay

--- index.js
+++ index.js
@@ -2,6 +2,32 @@
  * ShowMe.js - A simple library to tour people around your webpage.
  */
 
+const helpers = {
+	addTransparentOverlay: () => {
+		let elementHTML = `
+			<div id="showme-transparentoverlay" style="
+				position: fixed;
+				background: rgba(0, 0, 0, 0.7);
+				overflow-y: auto;
+				top: 0;
+				bottom: 0;
+				right: 0;
+				left: 0;
+				display: flex;
+				height: auto;
+				align-items: center;
+				z-index: 999;
+			"></div>
+		`;
+		if (!document.getElementById("showme-transparentoverlay"))
+			document.body.innerHTML += elementHTML;
+	},
+	removeTransparentOverlay: () => {
+		if (!document.getElementById("showme-transparentoverlay"))
+			document.getElementById("showme-transparentoverlay").remove();
+	},
+};
+
 class ShowMe {
 	constructError(code, message) {
 		const error = { message, code };
